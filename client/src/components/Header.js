abc3883e2831dcdd6adbe20700c53053319419f9
Anand Babu <babu.anand@pearson.com>
2019-10-01 15:27:28 +0530

    added some new features, stipe payment.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
-    renderContent(){
+    renderContent() {
         switch (this.props.auth) {
             case null:
                 return;
             case false:
-                return(
+                return (
                     <li><a href="/auth/google">Login with Google</a></li>
                 );
             default:
-                return <li><a href="/api/logout">Logout</a></li>
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="3" style={{margin:'0 10px'}}>Credits: {this.props.auth.credits}</li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="#" className="brand-logo">Emaily</a>
+                    <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">Emaily</Link>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         {this.renderContent()}
                     </ul>
@@ -27,7 +34,9 @@ class Header extends Component {
         );
     }
 }
+
 function mapStatetoProps({auth}) {
     return {auth};
 }
+
 export default connect(mapStatetoProps)(Header);
