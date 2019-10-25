9bc98f5602512bfe37c50131b72306fbaf105eea
Anand Babu <babu.anand@pearson.com>
2019-10-25 15:14:39 +0530

    dev.js config file

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '34517995695-viippvpafvopvp3ffkjf97r8l794prid.apps.googleusercontent.com',
+    googleClientSecret: 'Qj_-IHzFyF2v2p8fxJMImif4',
+    mongoURI: 'mongodb://emailydevuser:emailydevuser1@ds161517.mlab.com:61517/emaily-dev',
+    cookieKey: 'kakdhadhakjdlaid7a6780a07dasjdsbjashdasj',
+    stripePublishableKey: 'pk_test_nyy1RHk0fc5dCrkoIU8blHNe',
+    stripeSecretKey: 'sk_test_zUaOCaUvT3tEwl5wOPc5hihL'
+};
