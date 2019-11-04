43ef870d634e572cc47ac059e1fa393ef1b81139
Anand <anandkanatt@gmail.com>
2019-11-04 20:19:49 +0530

    routing logic fix

--- index.js
+++ index.js
@@ -26,6 +26,17 @@ app.use(passport.session());
 require('./routes/authroutes')(app);
 require('./routes/billingroutes')(app);
 
+if(process.env.NODE_ENV === 'production'){
+
+    //serving static content
+    app.use(express.static('client/build'));
+
+    //serve index.html if no other route exist
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
